1c61a03bf4d5c06f826646c69356e20ee8796b4d
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-10-04 18:46:36 +0200

    added areas controller logging

--- CSharp/Api/HackathonE1.Api/Controllers/ObservedAreasController.cs
+++ CSharp/Api/HackathonE1.Api/Controllers/ObservedAreasController.cs
@@ -4,6 +4,7 @@ using HackathonE1.Domain.Dtos.ObservedArea;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,23 +17,27 @@ namespace HackathonE1.Api.Controllers
 	[Route( "api/[controller]" )]
 	public class ObservedAreasController : ExtendedControllerBase
 	{
+		private readonly ILogger<ObservedAreasController> _logger;
 		private readonly IObservedAreasService _areasService;
 
-		public ObservedAreasController( IObservedAreasService areasService )
+		public ObservedAreasController( IObservedAreasService areasService, ILogger<ObservedAreasController> logger )
 		{
 			_areasService = areasService;
+			_logger = logger;
 		}
 
 		[HttpGet()]
 		[Authorize( Roles = "Admin" )]
 		public async Task<ActionResult<GetObservedAreaDto[]>> GetAreas()
 		{
+			_logger.LogInformation( $"User {UserName} requested all observed areas." );
 			return await _areasService.GetAreasAsync();
 		}
 
 		[HttpGet( "owned" )]
 		public async Task<ActionResult<GetObservedAreaDto[]>> GetOwnedAreas()
 		{
+			_logger.LogInformation( $"User {UserName} requested their observed areas." );
 			return await _areasService.GetUserAreasAsync( UserName );
 		}
 
@@ -43,9 +48,11 @@ namespace HackathonE1.Api.Controllers
 			var area = await _areasService.GetAreaAsync( id );
 			if ( area is null )
 			{
+				_logger.LogInformation( $"User {UserName} requested unknown observed area {id}." );
 				return NotFound( ProblemConstants.AreaNotFound );
 			}
 
+			_logger.LogInformation( $"User {UserName} requested observed area {id}." );
 			return area;
 		}
 
@@ -55,22 +62,26 @@ namespace HackathonE1.Api.Controllers
 			var area = await _areasService.GetAreaAsync( id, UserName );
 			if ( area is null )
 			{
+				_logger.LogInformation( $"User {UserName} requested not owned/unknown observed area {id}." );
 				return NotFound( ProblemConstants.AreaNotObserved );
 			}
 
+			_logger.LogInformation( $"User {UserName} requested their observed area {id}." );
 			return area;
 		}
 
 
-		[HttpPost]
+		[HttpPost( "owned" )]
 		public async Task<ActionResult<GetObservedAreaDto>> Observe( CreateObservedAreaDto areaDto )
 		{
 			var area = await _areasService.ObserveAreaAsync( areaDto, UserName );
 			if ( area is null )
 			{
+				_logger.LogInformation( $"User {UserName} failed creating observed area: already exists." );
 				return Conflict( ProblemConstants.AreaObserved );
 			}
 
+			_logger.LogInformation( $"User {UserName} created observed area {area.Id}." );
 			return CreatedAtAction( nameof( GetOwnedArea ), new { area.Id }, area );
 		}
 
@@ -82,9 +93,11 @@ namespace HackathonE1.Api.Controllers
 			var deleted = await _areasService.DeleteAreaAsync( id );
 			if ( !deleted )
 			{
+				_logger.LogInformation( $"User {UserName} requested deleting unknown observed area." );
 				return NotFound( ProblemConstants.AreaNotFound );
 			}
 
+			_logger.LogInformation( $"User {UserName} deleted observed area {id}." );
 			return NoContent();
 		}
 
@@ -94,9 +107,11 @@ namespace HackathonE1.Api.Controllers
 			var deleted = await _areasService.DeleteAreaAsync( id, UserName );
 			if ( !deleted )
 			{
+				_logger.LogInformation( $"User {UserName} requested deleting not their/unknown observed area." );
 				return NotFound( ProblemConstants.AreaNotObserved );
 			}
 
+			_logger.LogInformation( $"User {UserName} deleted their observed area {id}." );
 			return NoContent();
 		}
 

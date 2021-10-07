46df818fe2f8fba7765d742506160d5171b8236f
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-10-08 01:45:37 +0200

    AI done i hope so

--- Python/ObjectDetection/ImageService/src/file_upload_test.py
+++ Python/ObjectDetection/ImageService/src/file_upload_test.py
@@ -6,6 +6,9 @@ import requests
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
-files = {'file': ('test.jpg', open('images/test_image.jpg', 'rb'), content_type)}
+files = {'file': ('test.jpg', open('/Users/filip/GitHub/RubbishCam/Python/ObjectDetection/ImageService/src/images/test_image.jpg', 'rb'), content_type)}
 
-requests.post('http://localhost:5000/upload', files=files)
+r = requests.post('http://0.0.0.0:5000/upload', files=files)
+
+print(r)
+# print(r.json())
--- Python/ObjectDetection/ImageService/src/upload_file.py
+++ Python/ObjectDetection/ImageService/src/upload_file.py
@@ -16,25 +16,26 @@ def upload():
     
     try:
         file = request.files['file']
-    
+
         if file.filename == '':
             print('No selected file')
-    
+
         filename = secure_filename(file.filename)
-        file.save(os.path.join('images', filename))
-        
+        file.save(os.path.join('/app/src/images', filename))
+
         image = [tc.Image(path=f'/app/src/images/{filename}')]
         test = tc.SFrame({'image': image})
         predictions = model.predict(test)
         print(predictions)
         print(predictions[0])
-    
+
         return '', 200
-        
+
     except Exception as e:
         print('Error with file:', e)
-        return jsonify({'status': 500, 'title': 'Houston we may have a problem.'}), 500
+        return jsonify({'status': 500, 'title': 'Houston we may have a problem.', 'message': f'{e}'}), 500
     
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0')

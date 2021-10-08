45a0a5689ece010da8f85edc93a8c7c089c7db5e
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-10-08 23:34:24 +0200

    prepared page for hosting

--- CSharp/Website/HackathonE1.Website/Server/Program.cs
+++ CSharp/Website/HackathonE1.Website/Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace HackathonE1.Website.Server
@@ -21,6 +22,27 @@ namespace HackathonE1.Website.Server
 				.ConfigureWebHostDefaults( webBuilder =>
 				 {
 					 _ = webBuilder.UseStartup<Startup>();
+
+					 _ = webBuilder.UseKestrel( options =>
+					 {
+						 options.Listen( IPAddress.Any, GetKestrelPort() );
+					 } );
+
+					 _ = webBuilder.UseIIS();
 				 } );
+
+		public static int GetKestrelPort()
+		{
+			if ( int.TryParse( Environment.GetEnvironmentVariable( "PORT" ), out int port ) )
+			{
+				return port;
+			}
+			if ( int.TryParse( Environment.GetEnvironmentVariable( "ASPNETCORE_URLS" ), out port ) )
+			{
+				return port;
+			}
+
+			return 5001;
+		}
 	}
 }

eced4d2a150eacea4b10d9ad30d6a3fe2f0a125e
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2021-11-12 17:34:39 +0100

    added auth failure messages

--- backend/RubbishCam.Api/RubbishCam.Api/Auth/TokenAuthHandler.cs
+++ backend/RubbishCam.Api/RubbishCam.Api/Auth/TokenAuthHandler.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using RubbishCam.Data;
 using RubbishCam.Domain.Models;
 using System.Security.Claims;
+using System.Text;
 using System.Text.Encodings.Web;
 
 namespace RubbishCam.Api.Auth;
@@ -11,11 +16,21 @@ namespace RubbishCam.Api.Auth;
 public class TokenAuthHandler<T> : AuthenticationHandler<TokenOptions> where T : class, IAuthDataProvider
 {
 	private readonly IAuthDataProvider _provider;
+	private readonly ProblemDetailsFactory _detailsFactory;
+	private readonly IActionResultExecutor<ObjectResult> _executor;
 
-	public TokenAuthHandler( IAuthDataProvider provider, IOptionsMonitor<TokenOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock )
+	public TokenAuthHandler( IAuthDataProvider provider,
+						 IActionResultExecutor<ObjectResult> executor,
+						 ProblemDetailsFactory detailsFactory,
+						 IOptionsMonitor<TokenOptions> options,
+						 ILoggerFactory logger,
+						 UrlEncoder encoder,
+						 ISystemClock clock )
 		: base( options, logger, encoder, clock )
 	{
 		_provider = provider ?? throw new ArgumentNullException( nameof( provider ) );
+		_detailsFactory = detailsFactory ?? throw new ArgumentNullException( nameof( detailsFactory ) );
+		_executor = executor ?? throw new ArgumentNullException( nameof( executor ) );
 	}
 
 
@@ -38,7 +53,7 @@ public class TokenAuthHandler<T> : AuthenticationHandler<TokenOptions> where T :
 
 		if ( foundToken is null )
 		{
-			return AuthenticateResult.NoResult();
+			return AuthenticateResult.Fail( new TokenInvalidException() );
 		}
 
 		if ( foundToken.User is null )
@@ -46,13 +61,15 @@ public class TokenAuthHandler<T> : AuthenticationHandler<TokenOptions> where T :
 			throw new NullReferenceException();
 		}
 
+		if ( foundToken.ValidUntil < DateTimeOffset.Now )
+		{
+			return AuthenticateResult.Fail( new TokenExpiredException() );
+		}
+
 		Claim[] claims = GetClaims( foundToken.User ).ToArray();
-
-		ClaimsIdentity identity = new( claims, "Bearer" );
-
+		ClaimsIdentity identity = new( claims, Scheme.Name );
 		ClaimsPrincipal principal = new( identity );
-
-		AuthenticationTicket ticket = new( principal, "Bearer" );
+		AuthenticationTicket ticket = new( principal, Scheme.Name );
 
 		return AuthenticateResult.Success( ticket );
 
@@ -81,7 +98,6 @@ public class TokenAuthHandler<T> : AuthenticationHandler<TokenOptions> where T :
 
 		return token;
 	}
-
 	private static IEnumerable<Claim> GetClaims( UserModel user )
 	{
 		yield return new( ClaimTypes.Name, user.Uuid );
@@ -94,5 +110,92 @@ public class TokenAuthHandler<T> : AuthenticationHandler<TokenOptions> where T :
 		}
 	}
 
+
+	protected override async Task HandleChallengeAsync( AuthenticationProperties properties )
+	{
+		Response.StatusCode = 401;
+
+		AuthenticateResult authResult = await HandleAuthenticateOnceSafeAsync();
+		if ( !authResult.None && authResult.Failure is null )
+		{
+			return;
+		}
+
+		// https://datatracker.ietf.org/doc/html/rfc6750#section-3.1
+		StringBuilder wwwauth = new( $"{Scheme.Name} realm=\"RubbishCamDefault\"" );
+
+		if ( authResult.None )
+		{
+			Response.Headers.Append( HeaderNames.WWWAuthenticate, wwwauth.ToString() );
+			return;
+		}
+
+		string? detail = CreateErrorDescription( authResult.Failure! );
+		if ( detail is not null )
+		{
+			_ = wwwauth.Append( "error=\"invalid_token\", "
+					   + $"error_description=\"{detail}\"" );
+		}
+
+		Response.Headers.Append( HeaderNames.WWWAuthenticate, wwwauth.ToString() );
+
+		await FillResponse( Response.StatusCode, detail );
+
+	}
+
+	protected override async Task HandleForbiddenAsync( AuthenticationProperties properties )
+	{
+		Response.StatusCode = 403;
+
+		// https://datatracker.ietf.org/doc/html/rfc6750#section-3.1
+		Response.Headers.Append( HeaderNames.WWWAuthenticate, $"{Scheme.Name} realm=\"RubbishCamDefault\", error=\"insufficient_scope\"" );
+
+		await FillResponse( Response.StatusCode, "Insufficient scope" );
+	}
+
+
+	private async Task FillResponse( int code, string? detail )
+	{
+		ActionContext actionContext = new( Context, Context.GetRouteData(), new ActionDescriptor() );
+
+		ProblemDetails problemDetails = _detailsFactory.CreateProblemDetails( Context, code, detail: detail );
+		ObjectResult result = new( problemDetails )
+		{
+			StatusCode = problemDetails.Status
+		};
+
+		await _executor.ExecuteAsync( actionContext, result );
+	}
+	private static string? CreateErrorDescription( Exception authFailure )
+	{
+		return authFailure switch
+		{
+			TokenInvalidException =>
+				"The token is invalid",
+			TokenExpiredException =>
+				"The token is expired",
+			_ => null
+		};
+	}
+
+
+
+	[Serializable]
+	public class TokenInvalidException : Exception
+	{
+		public TokenInvalidException() { }
+		protected TokenInvalidException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
+	}
+
+	[Serializable]
+	public class TokenExpiredException : Exception
+	{
+		public TokenExpiredException() { }
+		protected TokenExpiredException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
+	}
 
 }

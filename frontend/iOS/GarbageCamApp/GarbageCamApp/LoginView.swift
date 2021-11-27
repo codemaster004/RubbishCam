522b5bcf29a125b4846f1be123e2c7b89627e6b2
Filip Dabkowski <filip.dabkowski@gmail.com>
2021-11-27 16:18:17 +0100

    Add SignUpView
    adding sign up view and repair problem with not visible button

--- frontend/iOS/GarbageCamApp/GarbageCamApp/LoginView.swift
+++ frontend/iOS/GarbageCamApp/GarbageCamApp/LoginView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var username = ""
-    @State var password = ""
+    @State var email: String = ""
+    @State var password: String = ""
     
     var body: some View {
         ZStack {
@@ -21,7 +21,7 @@ struct LoginView: View {
                     UIApplication.shared.endEditing()
                 }
             
-            VStack(spacing: 25) {
+            VStack(spacing: 10) {
                 VStack {
                     Image("Logo")
                         .resizable()
@@ -34,52 +34,80 @@ struct LoginView: View {
                         .font(.system(size: 24))
                 }
                 
-                // Username Input
-                VStack(alignment: .leading, spacing: 7) {
-                    Text("Username")
-                        .foregroundColor(Color("TextSecondary"))
-                        .fontWeight(.medium)
+                VStack(spacing: 20) {
+                    // Email Input
+                    AuthTextField(text: $email, hint: "Email")
                     
-                    TextField("Enter Your Username", text: $username, onCommit:  {
+                    // Password Input
+                    AuthSecretField(text: $password)
+                }
+                    
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text("Don't have an account?")
+                            .foregroundColor(Color("TextSecondary"))
+                            .fontWeight(.medium)
+                        
+                        Text("Sign up")
+                            .foregroundColor(Color("MainColor"))
+                            .fontWeight(.medium)
+                    }
+                    
+                    MainButton(text: "Get Started") {
+                        print("Login in")
                         UIApplication.shared.endEditing()
-                    })
-                    .padding()
-                    .frame(height: 59)
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color("AccentGrey"), lineWidth: 1)
-                    )
+                    }
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical)
+        }
+    }
+}
+
+struct SignUpView: View {
+    
+    @State var email: String = ""
+    @State var fullname: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        ZStack {
+            
+            Color("BgcGray")
+                .ignoresSafeArea()
+                .onTapGesture {
+                    UIApplication.shared.endEditing()
+                }
+            
+            VStack(spacing: 10) {
+                VStack {
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    .frame(width: 130)
+                    
+                    Text("Log In")
+                        .foregroundColor(Color("MainColor"))
+                        .fontWeight(.bold)
+                        .font(.system(size: 24))
                 }
                 
-                // Password Input
-                VStack(alignment: .leading, spacing: 7) {
-                    Text("Password")
-                        .foregroundColor(Color("TextSecondary"))
-                        .fontWeight(.medium)
+                VStack(spacing: 20) {
+                    // Full name Input
+                    AuthTextField(text: $fullname, hint: "Full name")
                     
-                    SecureField("Create Password", text: $password, onCommit:  {
-                        UIApplication.shared.endEditing()
-                    })
-                        .padding(.horizontal)
-                    .frame(height: 59)
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color("AccentGrey"), lineWidth: 1)
-                    )
+                    // Email Input
+                    AuthTextField(text: $email, hint: "Email")
                     
-                    HStack {
-                        Spacer()
-                        
-                        Text("Forgot password?")
-                            .foregroundColor(Color("MainColor"))
-                    }
+                    // Password Input
+                    AuthSecretField(text: $password)
                 }
                 
                 Spacer()
+                    .frame(minHeight: 0)
                 
                 VStack {
                     HStack {
@@ -106,7 +134,10 @@ struct LoginView: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        Group {
+            LoginView()
+            SignUpView()
+        }
     }
 }
 
@@ -142,5 +173,64 @@ struct MainButton: View {
             .shadow(color: Color("MainColor").opacity(0.7), radius: 9, x: 0, y: 4)
         }
         .buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct AuthTextField: View {
+    
+    @Binding var text: String
+    
+    let hint: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 7) {
+            Text(hint)
+                .foregroundColor(Color("TextSecondary"))
+                .fontWeight(.medium)
+            
+            TextField("Enter Your \(hint)", text: $text, onCommit:  {
+                UIApplication.shared.endEditing()
+            })
+                .padding()
+                .frame(height: 50)
+                .background(Color.white)
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color("AccentGrey"), lineWidth: 1)
+                )
+        }
+    }
+}
+
+struct AuthSecretField: View {
+    
+    @Binding var text: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 7) {
+            Text("Password")
+                .foregroundColor(Color("TextSecondary"))
+                .fontWeight(.medium)
+            
+            SecureField("Create Password", text: $text, onCommit:  {
+                UIApplication.shared.endEditing()
+            })
+                .padding(.horizontal)
+                .frame(height: 50)
+                .background(Color.white)
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color("AccentGrey"), lineWidth: 1)
+                )
+            
+            HStack {
+                Spacer()
+                
+                Text("Forgot password?")
+                    .foregroundColor(Color("MainColor"))
+            }
+        }
     }
 }
